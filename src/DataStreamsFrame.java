03350b821d43358868fdc0278118d1fbda3dd236
Szalapbp <szalapbp@mail.uc.edu>
2025-04-09 22:42:46 -0400

    the logic for the program is now done

--- src/DataStreamsFrame.java
+++ src/DataStreamsFrame.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
+import java.io.*;
+import java.nio.file.*;
+import java.util.stream.Stream;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class DataStreamsFrame extends JFrame
 {
@@ -37,6 +41,9 @@ public class DataStreamsFrame extends JFrame
         commandPnl.add(searchBtn);
         commandPnl.add(quitBtn);
 
+        originalWordsScroll.setPreferredSize(new Dimension(300, 400));
+        returnWordsScroll.setPreferredSize(new Dimension(300, 400));
+
         add(commandPnl, BorderLayout.NORTH);
         add(textPnl, BorderLayout.SOUTH);
 
@@ -48,11 +55,48 @@ public class DataStreamsFrame extends JFrame
 
     private void loadFile()
     {
-
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(this);
+        if(result == JFileChooser.APPROVE_OPTION)
+        {
+            loadedFile = fileChooser.getSelectedFile();
+            try(Stream<String> lines = Files.lines(Paths.get(loadedFile.getAbsolutePath())))
+            {
+                originalWords.setText("");
+                lines.forEach(line -> originalWords.append(line + "\n"));
+            } catch (IOException ex)
+            {
+                JOptionPane.showMessageDialog(this, "Error loading file: "
+                        + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 
     private void searchFile()
     {
+        if(loadedFile == null){
+            JOptionPane.showMessageDialog(this, "No File Selected",
+                    "Warning", JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+        String searchString = searchWord.getText();
+        if(searchString.isEmpty())
+        {
+            JOptionPane.showMessageDialog(this, "Search string is empty! Type a Word!",
+                    "Warning", JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+        try(Stream<String> lines = Files.lines(Paths.get(loadedFile.getAbsolutePath())))
+        {
+            returnWords.setText("");
+            lines.filter(line -> line.contains(searchString))
+                    .forEach(line -> returnWords.append(line + "\n"));
+
+        }catch(IOException ex)
+        {
+            JOptionPane.showMessageDialog(this, "Error searching file: " + ex.getMessage(),
+                    "Error", JOptionPane.ERROR_MESSAGE);
+        }
 
     }
 }

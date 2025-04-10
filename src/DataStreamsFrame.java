8843aff165b0195ffc8acdb95b0277b4fd1c42db
Szalapbp <szalapbp@mail.uc.edu>
2025-04-09 22:06:24 -0400

    created my files and started adding functionality

---
+++ src/DataStreamsFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class DataStreamsFrame extends JFrame
+{
+    JTextField searchWord;
+    JTextArea originalWords, returnWords;
+    JButton searchBtn, loadFileBtn, quitBtn;
+    JPanel mainPnl, commandPnl, textPnl;
+
+    public DataStreamsFrame()
+    {
+
+    }
+}
